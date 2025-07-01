c45023658b0f7d821c854891eecf0373004d58a5
Eduardo-Domingues <eduardo.domingues.pereira@escola.pr.gov.br>
2025-07-01 10:33:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Motorzão',
+    'Qual moto é conhecida como "tanque de guerra"?',
+    'A famosa titan 125, a cg 99'
+)
+
+criaCartao(
+    'Motorzão',
+    'Qual moto é conhecida por ser uma das melhores da Honda?',
+    'É a cg 150 carburada de 2004 a 2008'
+)
+
+criaCartao(
+    'Motorzão',
+    'Qual a preparação mais mentirosa dos racha?',
+    'É o 4mm'
+)
+
+criaCartao(
+    'Motorzão',
+    'O que faz o motor queimar combustível?',
+    'A vela'
+)
